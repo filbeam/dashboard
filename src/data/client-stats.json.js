d5def0a326fb2de5f487268d8458e24778816c9d
Srdjan <stankovic.srdjo@gmail.com>
2025-11-12 14:41:57 +0100

    fix: client stats overview query (#26)
    * fix: separate client stats and quota queries into two subqueries
    
    * fix: support negative numbers in formatBytesIEC
    
    * chore: simplify formatBytesIEC
    
    * chore: simplify formatBytesIEC

--- src/data/client-stats.json.js
+++ src/data/client-stats.json.js
@@ -2,24 +2,42 @@ import { query } from './cloudflare-client.js'
 
 const response = await query(
   `
+  WITH client_stats AS (
+    SELECT
+      cs.payer_address,
+      COUNT(rl.id) AS total_requests,
+      SUM(CASE WHEN rl.cache_miss THEN 1 ELSE 0 END) AS cache_miss_requests,
+      SUM(rl.egress_bytes) AS total_egress_bytes,
+      SUM(CASE WHEN rl.cache_miss THEN rl.egress_bytes ELSE 0 END) AS cache_miss_egress_bytes
+    FROM
+      retrieval_logs rl
+    JOIN
+      data_sets cs ON cs.id = rl.data_set_id
+    GROUP BY
+      cs.payer_address
+  ),
+  client_quotas AS (
+    SELECT
+      cs.payer_address,
+      SUM(cseqs.cdn_egress_quota) AS remaining_cdn_egress_bytes,
+      SUM(cseqs.cache_miss_egress_quota) AS remaining_cache_miss_egress_bytes
+    FROM
+      data_sets cs
+    JOIN
+      data_set_egress_quotas cseqs ON cs.id = cseqs.data_set_id
+    GROUP BY
+      cs.payer_address
+  )
   SELECT
-    ds.payer_address,
-    COUNT(*) AS total_requests,
-    SUM(CASE WHEN rl.cache_miss THEN 1 ELSE 0 END) AS cache_miss_requests,
-    SUM(rl.egress_bytes) AS total_egress_bytes,
-    SUM(CASE WHEN rl.cache_miss THEN egress_bytes ELSE 0 END) AS cache_miss_egress_bytes,
-    SUM(dseqs.cdn_egress_quota) AS remaining_cdn_egress_bytes,
-    SUM(dseqs.cache_miss_egress_quota) AS remaining_cache_miss_egress_bytes
+    cs.*,
+    cq.remaining_cdn_egress_bytes,
+    cq.remaining_cache_miss_egress_bytes
   FROM
-    retrieval_logs rl
-  JOIN
-    data_sets ds ON ds.id = rl.data_set_id
-  JOIN
-    data_set_egress_quotas dseqs ON ds.id = dseqs.data_set_id
-  GROUP BY
-    ds.payer_address
+    client_stats cs
+  LEFT JOIN
+    client_quotas cq ON cs.payer_address = cq.payer_address
   ORDER BY
-    total_requests DESC;
+    cs.total_requests DESC;
 `,
   [],
 )
