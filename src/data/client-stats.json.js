f1cd80fc2f5d05e04785f633cd82c7d8fc04422f
Julian Gruber <julian@juliangruber.com>
2025-11-12 13:18:00 +0100

    fix query

--- src/data/client-stats.json.js
+++ src/data/client-stats.json.js
@@ -8,12 +8,14 @@ const response = await query(
     SUM(CASE WHEN rl.cache_miss THEN 1 ELSE 0 END) AS cache_miss_requests,
     SUM(rl.egress_bytes) AS total_egress_bytes,
     SUM(CASE WHEN rl.cache_miss THEN egress_bytes ELSE 0 END) AS cache_miss_egress_bytes,
-    SUM(ds.cdn_egress_quota) AS remaining_cdn_egress_bytes,
-    SUM(ds.cache_miss_egress_quota) AS remaining_cache_miss_egress_bytes
+    SUM(dseqs.cdn_egress_quota) AS remaining_cdn_egress_bytes,
+    SUM(dseqs.cache_miss_egress_quota) AS remaining_cache_miss_egress_bytes
   FROM
     retrieval_logs rl
   JOIN
     data_sets ds ON ds.id = rl.data_set_id
+  JOIN
+    data_set_egress_quotas dseqs ON ds.id = dseqs.data_set_id
   GROUP BY
     ds.payer_address
   ORDER BY
