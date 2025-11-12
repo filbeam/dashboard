96a46a8721cf40d48b56dbf86a3664c85e47935a
Srdjan S <stankovic.srdjo@gmail.com>
2025-11-12 10:20:20 -0300

    chore: simplify formatBytesIEC

--- src/utils/bytes.js
+++ src/utils/bytes.js
@@ -1,13 +1,9 @@
 export function formatBytesIEC(bytes) {
   if (bytes === 0) return '0 B'
 
-  const isNegative = bytes < 0
-  const absoluteBytes = Math.abs(bytes)
-
   const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']
-  const index = Math.floor(Math.log2(absoluteBytes) / 10)
-  const converted = absoluteBytes / Math.pow(2, index * 10)
+  const index = Math.floor(Math.log2(Math.abs(bytes)) / 10)
+  const converted = Math.abs(bytes) / Math.pow(2, index * 10)
 
-  const formatted = `${converted.toFixed(2)} ${units[index]}`
-  return isNegative ? `-${formatted}` : formatted
+  return `${bytes < 0 ? '-' : ''}${converted.toFixed(2)} ${units[index]}`
 }
